4bf4d9b3c61bce1ae4fc17bc9b34ef434a88f7b4
contra3000 <contra3000@gmail.com>
2022-12-12 21:54:59 -0300

    Swagger security solved

--- src/main/java/com/alkemy/wallet/WalletApplication.java
+++ src/main/java/com/alkemy/wallet/WalletApplication.java
@@ -1,12 +1,17 @@
 package com.alkemy.wallet;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(type = SecuritySchemeType.APIKEY, name = "Bearer", in = SecuritySchemeIn.HEADER)
 @OpenAPIDefinition(
         info = @Info(title = "Wallet API Messirve LTD",
                 version = "1.0",
@@ -14,8 +19,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 contact = @Contact(name = "Messirve LTD",
                         url = "https://discord.gg/CTDS6GUA",
                         email = "messirveltd@gmail.com"),
-                termsOfService = "https://developers.google.com/terms")
-)
+                termsOfService = "https://developers.google.com/terms"),
+        security = @SecurityRequirement(name = "Bearer"))
 public class WalletApplication {
 
     public static void main(String[] args) {
